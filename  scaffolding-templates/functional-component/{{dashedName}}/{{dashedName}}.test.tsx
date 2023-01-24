e251affce3b550ca17a1524e3a6263b75f9e5e53
Arie Goldkin <ariegoldkin@Aries-MacBook-Pro-2.local>
2023-01-25 00:09:12 +0200

    another upload

---
+++  scaffolding-templates/functional-component/{{dashedName}}/{{dashedName}}.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import * as ReactDOM from "react-dom";
+import { {{pascalCaseName}} } from "./{{dashedName}}";
+
+test("renders {{pascalCaseName}}", () => {
+  ReactDOM.render(<{{pascalCaseName}} />);
+});
