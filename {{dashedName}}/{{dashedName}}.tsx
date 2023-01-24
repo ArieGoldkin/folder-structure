cfce0e573b2173db2b1049f182e1c47d82a30a45
Arie Goldkin <ariegoldkin@Aries-MacBook-Pro-2.local>
2023-01-24 23:45:54 +0200

    first commit

---
+++ {{dashedName}}/{{dashedName}}.tsx
@@ -0,0 +1,9 @@
+import React from  "react";
+
+import { {{snakeCaseName}} } from "./{{dashedName}}.module.css";
+
+export type {{pascalCaseName}}Props = {}
+
+export const {{pascalCaseName}} = (props:{{pascalCaseName}}Props) => {
+    return <div className={ {{snakeCaseName}} }>{{pascalCaseName}}</div>
+}
