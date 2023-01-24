1df628283a648e741e88804c1303fad7f1da31c2
Arie Goldkin <ariegoldkin@Aries-MacBook-Pro-2.local>
2023-01-25 00:11:50 +0200

    aaa

--- functional-component/{{dashedName}}/{{dashedName}}.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import * as ReactDOM from "react-dom";
-import { {{pascalCaseName}} } from "./{{dashedName}}";
-
-test("renders {{pascalCaseName}}", () => {
-  ReactDOM.render(<{{pascalCaseName}} />);
-});
--- functional-component/{{dashedName}}/{{dashedName}}.tsx
+++
@@ -1,9 +0,0 @@
-import React from  "react";
-
-import { {{snakeCaseName}} } from "./{{dashedName}}.module.css";
-
-export type {{pascalCaseName}}Props = {}
-
-export const {{pascalCaseName}} = (props:{{pascalCaseName}}Props) => {
-    return <div className={ {{snakeCaseName}} }>{{pascalCaseName}}</div>
-}
